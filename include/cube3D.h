604753523d87832cc27b0de40db88d7e2c558c8e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-09-25 19:48:31 +0200

    Moving the cube

--- include/cube3D.h
+++ include/cube3D.h
@@ -6,16 +6,28 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 18:35:59 by juestrel          #+#    #+#             */
-/*   Updated: 2024/09/24 18:57:26 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/09/25 19:33:59 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE3D_H
 # define CUBE3D_H
+# ifndef WIDTH
+#  define WIDTH 1920
+# endif
+# ifndef HEIGHT
+#  define HEIGHT 1080
+# endif
 
 # include "../MLX42/include/MLX42/MLX42.h"
 # include <fcntl.h>
 # include <stdarg.h>
 # include <stdlib.h>
+
+typedef struct s_tests
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+}				t_tests;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,29 +6,54 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 18:30:36 by juestrel          #+#    #+#             */
-/*   Updated: 2024/09/24 19:17:13 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/09/25 19:48:04 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cube3D.h"
 #include <stdio.h> //Borrar luego
 
-static void close_window(void *param)
+static void cube_draw(mlx_image_t *img)
 {
-    if(mlx_is_key_down((mlx_t *)param, MLX_KEY_ESCAPE))
-        mlx_close_window((mlx_t *)param);
+    unsigned int color = 255 << 24 | 255 << 16 | 0 << 8 | 255;
+    unsigned int size = 16;
+
+    for (unsigned int y = (HEIGHT/2) - size; y < HEIGHT/2; y++)
+    {
+        for (unsigned int x = (WIDTH/2) - size; x < WIDTH/2; x++) 
+            mlx_put_pixel(img, x, y, color);
+    }
+}
+
+static void hooks(void *param)
+{
+   t_tests *test = (t_tests *)param;
+
+    if(mlx_is_key_down(test->mlx, MLX_KEY_ESCAPE))
+        mlx_close_window(test->mlx);
+    if (mlx_is_key_down(test->mlx, MLX_KEY_W))
+        test->img->instances[0].y -= 5;
+    if (mlx_is_key_down(test->mlx, MLX_KEY_S))
+        test->img->instances[0].y += 5;
+    if (mlx_is_key_down(test->mlx, MLX_KEY_D))
+        test->img->instances[0].x += 5;
+    if (mlx_is_key_down(test->mlx, MLX_KEY_A))
+        test->img->instances[0].x -= 5;
 }
 
 int	main(void)
 {
-    mlx_t *mlx = mlx_init(1920, 1080, "cube3D", true);
-    if (!mlx)
+    t_tests main;
+
+    main.mlx = mlx_init(1920, 1080, "cube3D", true);
+    if (!main.mlx)
         return(1);
-    mlx_image_t *img = mlx_new_image(mlx, 1000, 1000);
-    if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+    main.img = mlx_new_image(main.mlx, WIDTH, HEIGHT);
+    if (!main.img || (mlx_image_to_window(main.mlx, main.img, 0, 0) < 0))
 		return (1);
-    mlx_loop_hook(mlx, close_window, mlx);
-    mlx_loop(mlx);
-	mlx_terminate(mlx);
+    cube_draw(main.img);
+    mlx_loop_hook(main.mlx, hooks, &main);
+    mlx_loop(main.mlx);
+	mlx_terminate(main.mlx);
 	return (0);
 }

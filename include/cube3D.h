59c2da384d5616c9c97f33a7d7b40cb419089c81
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-10-01 20:18:10 +0200

    Created raycast struct

--- include/cube3D.h
+++ include/cube3D.h
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 18:35:59 by juestrel          #+#    #+#             */
-/*   Updated: 2024/09/25 19:33:59 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/10/01 20:16:20 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,29 @@ typedef struct s_tests
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 }				t_tests;
+
+typedef struct s_ray
+{
+	double		pos_Y;
+	double		pos_X;
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
+	double		camera_x;
+	double		ray_dir_x;
+	double		ray_dir_y;
+	int			map_x;
+	int			map_y;
+	double		side_dist_x;
+	double		side_dist_y;
+	double		delta_dist_x;
+	double		delta_dist_y;
+	double		perpWallDist;
+	int			step_x;
+	int			step_y;
+	bool		hit;
+	int			side;
+}				t_ray;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 18:30:36 by juestrel          #+#    #+#             */
-/*   Updated: 2024/09/25 19:51:55 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/10/01 20:17:57 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cube3D.h"
 
-static void cube_draw(mlx_image_t *img)
+/*static void cube_draw(mlx_image_t *img)
 {
     unsigned int color = 255 << 24 | 255 << 16 | 0 << 8 | 255;
     unsigned int size = 16;
@@ -40,6 +40,30 @@ static void hooks(void *param)
         test->img->instances[0].x += 5;
     if (mlx_is_key_down(test->mlx, MLX_KEY_A))
         test->img->instances[0].x -= 5;
+}*/
+
+static t_ray init_ray(void) 
+{
+    t_ray raycast;
+
+    raycast.dir_x = -1;
+    raycast.dir_y = 0;
+    raycast.plane_x = 0;
+    raycast.plane_y = 0.66;
+    raycast.camera_x = 0;
+    raycast.ray_dir_x = 0;
+    raycast.ray_dir_y = 0;
+    raycast.map_x = 0;
+    raycast.map_y = 0;
+    raycast.side_dist_x = 0;
+    raycast.side_dist_y = 0;
+    raycast.delta_dist_x = 0;
+    raycast.delta_dist_y = 0;
+    raycast.perpWallDist = 0;
+    raycast.step_x = 0;
+    raycast.step_y = 0;
+    raycast.hit = false;
+    raycast.side = 0;
 }
 
 int	main(void)
@@ -52,8 +76,9 @@ int	main(void)
     main.img = mlx_new_image(main.mlx, 16, 16);
     if (!main.img || (mlx_image_to_window(main.mlx, main.img, 0, 0) < 0))
 		return (1);
-    cube_draw(main.img);
-    mlx_loop_hook(main.mlx, hooks, &main);
+    //cube_draw(main.img);
+    //mlx_loop_hook(main.mlx, hooks, &main);
+    t_ray ray = init_ray();
     mlx_loop(main.mlx);
 	mlx_terminate(main.mlx);
 	return (0);
